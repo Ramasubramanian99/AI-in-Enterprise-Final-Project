8745769ac9a70038552940ad8ad743b99d94073a
Ramasubramanian99 <d.ramasubramanian.1997@gmail.com>
2022-04-11 15:55:27 -0400

    Modify requirements.txt

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, url_for, redirect, render_template, jsonify
-import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-import pandas as pd
 import pickle
 import json
 
